304ee07891d5975e3ae48cf7e1255a93150c2a8a
SkelSec <info@skelsec.com>
2018-10-14 01:55:50 +0200

    Update socksOhttp/socksohttp/modules/socks5.py

--- socksOhttp/socksohttp/modules/socks5.py
+++ socksOhttp/socksohttp/modules/socks5.py
@@ -1102,7 +1102,8 @@ class Socks5ModuleServer(CommsModule):
 			sock.bind((self.listen_ip, 0))
 
 			server = await asyncio.start_server(self.handle_client, sock=sock)
-			logger.info('%s is now listening on %s:%d' % (self.module_name, sock.getsockname()))
+			addrs = '%s:%d' % sock.getsockname()
+			logger.info('%s is now listening on %s' % (self.module_name, addrs))
 			async with server:
 				await server.serve_forever()
 		except Exception as e:
